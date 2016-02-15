8e1076a9bf9ecccd6e463859eac545bdf2c77dad
ayiemba <ayiembaelvis@gmail.com>
2016-02-16 01:53:18 +0300

    layerswitcher added

--- app.js
+++ app.js
@@ -8,26 +8,23 @@
 
 	/* create layer and source instances */
 
+	//layers array to hold all layers
+	var layersArray = [];
+
 	//osm base layers
 	var osmlayer = new ol.layer.Tile({
 		source: new ol.source.MapQuest({layer:'osm'}),
 		//name: 'Openstreet map'
-
 	});
 	var satellite = new ol.layer.Tile({
 		source: new ol.source.MapQuest({layer:'sat'})
 	})
-	//esri base map
-	/*var esriBase = new ol.layer.Tile({
-		source: new ol.source.XYZ({
-			url:'http://server.arcgisonline.com/ArcGIS/rest/services' + 'World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
-		})
-	})*/
 
-	//layers from geoserver
+
+	//subcounties boundaries layer
 	var boundarySource = new ol.source.TileWMS({
 		url: 'http://localhost:8080/geoserver/wms',
-		params: { 'LAYERS':'cpt:nairobi_county',transparent:'true'},
+		params: { 'LAYERS':'cpt:NairobiSubCounties',transparent:'true'},
 		serverType:'geoserver'
 	});
 
@@ -36,93 +33,63 @@
 		name: 'County boundary'
 	});
 
-	/*var townsSource = new ol.source.TileWMS({
-		url: 'http://localhost:8080/geoserver/wms',
-		params:{'LAYERS':'cpt:nairobi_towns'},
-		serverType:'geoserver' 
-	})
-	//using towns layer for testing application before development projects data is obtained
-	var countyTowns = new ol.layer.Tile({
-		source: townsSource,
-		name: 'Towns'
-	});*/
 
 	/*project layers defined below*/
 	var allProjectsSource = new ol.source.TileWMS({
 		url: 'http://localhost:8080/geoserver/wms',
-		params:{'LAYERS':'cpt:projects'},
+		params:{'LAYERS':'cpt:allProjects'},
 		serverType:'geoserver'
 	});
 	var allProjects = new ol.layer.Tile({
 		source: allProjectsSource,
 		name: 'All projects'
 	});
+	allProjects.setVisible(false);
 
-	
+	var ongoingProjectsSource = new ol.source.TileWMS({
+		url: 'http://localhost:8080/geoserver/wms',
+		params: { 'LAYERS':'cpt:ongoingProjects'},
+		serverType:'geoserver'
+	});
 	var ongoingProjects = new ol.layer.Tile({ 
-		source: new ol.source.TileWMS({
-			url: 'http://localhost:8080/geoserver/wms',
-			params: { 'LAYERS':'cpt:ongoing'},
-			serverType:'geoserver'
-		}),
+		source: ongoingProjectsSource,
 		name: 'Ongoing projects'
 	});
+	ongoingProjects.setVisible(false);
 
+	var completedProjectsSource = new ol.source.TileWMS({
+		url: 'http://localhost:8080/geoserver/wms',
+		params: { 'LAYERS':'cpt:completedProjects'},
+		serverType:'geoserver'
+	});
 	var completedProjects = new ol.layer.Tile({ 
-		source: new ol.source.TileWMS({
-			url: 'http://localhost:8080/geoserver/wms',
-			params: { 'LAYERS':'cpt:completed'},
-			serverType:'geoserver'
-		}),
+		source: completedProjectsSource,
 		name: 'Completed projeccts'
 	});
+	completedProjects.setVisible(false);
+
+	var proposedProjectsSource = new ol.source.TileWMS({
+		url: 'http://localhost:8080/geoserver/wms',
+		params: { 'LAYERS':'cpt:proposedProjects'},
+		serverType:'geoserver'
+	});
 	var proposedProjects = new ol.layer.Tile({ 
-		source: new ol.source.TileWMS({
-			url: 'http://localhost:8080/geoserver/wms',
-			params: { 'LAYERS':'cpt:proposed'},
-			serverType:'geoserver'
-		}),
+		source: proposedProjectsSource,
 		name: 'Proposed projects'
 	});
-	/*var stalledProjects = new ol.layer.Tile({ 
-		source: new ol.source.TileWMS({
-			url: 'http://localhost:8080/geoserver/wms',
-			params: { 'LAYERS':'homabay:homabay_constituencies'},//to give appropriate params
-			serverType:'geoserver'
-		})
-	});*/
+	proposedProjects.setVisible(false);
 	
+	//adding layers to the layers array
+	layersArray.push(osmlayer); //0
+	layersArray.push(countybnd); //1
+	layersArray.push(ongoingProjects); //2
+	layersArray.push(proposedProjects); //3
+	layersArray.push(completedProjects); //4
+	layersArray.push(allProjects); //5
 
-	//var minExtent = ol.proj.transform([33.90,-0.95],'EPSG:4326','EPSG:3857')
-	//var extent = [33.90,-0.95,35.06,-0.36]
-
-	//Creating base maps layer group
-	var baseMaps = new ol.layer.Group({
-		'title':'Base maps',
-		layers:[osmlayer]//only one base map used
-	})
-	//Creating overlay group
-	//NB: To write code to asynchronously change map layers based on choice from user
-	//use only one base map
-	/*
-		get checkboxes asynchronously
-		check value of each check box
-		update map overlay asynchronously
-
-		//Tile.getSource().on('change', function(evt){
-			var source = evt.target;
-			if (source.getState()=== 'ready'){
-				var numFeatures = source.getFeatures().length;
-			}
-		})//
-	*/
-
-	var overLays = new ol.layer.Group({
-		'title':'Projecst',
-		layers:[countybnd,allProjects,proposedProjects,ongoingProjects,completedProjects,]
-	})
 	
-	//Configure view properties for map instance 
+	
+	//configure view properties for map instance 
 	var view = new ol.View({
 		projection: 'EPSG:3857',//because osm base layer is in 'ESPG:3857'
 			center: ol.proj.transform([36.87,-1.28], 'EPSG:4326','EPSG:3857'),
@@ -130,29 +97,10 @@
 	});
 
 	
-    //Access pop-up elements. 
+    //access pop-up elements. 
 	var container = document.getElementById('popup');
 	var content = document.getElementById('popup-content');
 	var closer = document.getElementById('popup-closer');
-
-	//displays coordinates on map
-	var mousePositionControl = new ol.control.MousePosition({
-          className: 'custom-mouse-position',
-          target: document.getElementById('location'),
-          projection:'EPSG:4326' ,
-          coordinateFormat: ol.coordinate.createStringXY(2),
-          undefinedHTML: '&nbsp;'
-        });
-
-	//zoom to extent
-	/*var zoomToExtentControl = new ol.control.ZoomToExtent({
-		projection: 'EPSG:4326',
-		extent: extent
-	});*/
-	
-	//scale line control
-	//var scale = new ol.control.ScaleLine({units:'degrees'})
-
 
 	//click handler to hide/close pop-up
 	closer.onclick = function() {
@@ -170,16 +118,30 @@
   		}
 	}));
 
+	//control to display map coordinates
+	var mousePositionControl = new ol.control.MousePosition({
+          className: 'custom-mouse-position',
+          target: document.getElementById('location'),
+          projection:'EPSG:4326' ,
+          coordinateFormat: ol.coordinate.createStringXY(2),
+          undefinedHTML: '&nbsp;'
+        });
+
 	
 	//create the map instance
 	var map = new ol.Map({
 		renderer: 'canvas',//force renderer to be used
 		controls: ol.control.defaults().extend([mousePositionControl]),
-		layers: [baseMaps,overLays],
+		layers: layersArray,
 		overlays: [overlay],
 		target:'map',
 		view: view
 	});
+
+	//function to manage layer visibility
+	function switchlayer(e){
+		layersArray[e.value].setVisible(e.checked);
+	}
 
 
 	/*this function changes the cursor to a pointer when cursor is on map*/
@@ -202,59 +164,20 @@
     	}
 	});
 
-	/*map.on('pointermove', function(evt) {
-		if (evt.dragging) {
-		    return;
-		}
-		var pixel = map.getEventPixel(evt.originalEvent);
-		var hit = map.forEachLayerAtPixel(pixel, function(layer) {
-		    return true;
-		});
-		map.getTargetElement().style.cursor = hit ? 'pointer' : '';
-	});*/
 
-	/*Add a click handler to the map to render the popup.*/
+	/*adds a click handler to the map to render the popup.*/
 	map.on('singleclick', function(evt) {
 		var coordinate = evt.coordinate;
 		var viewResolution = (view.getResolution());
-
-		/*var featureinfo;
-		var layers = map.getLayers()
-		for(i=0; i<=layers.length; i++){
-			if(i==countyTowns){
-				featureinfo = townsSource.getGetFeatureInfoUrl(
-		 	    coordinate, viewResolution,'EPSG:3857', 
-		 	    {'INFO_FORMAT': 'application/json'});
-			}
-			else if(i==countybnd){
-				featureinfo = featureinfo;
-			}
-			else{
-				featureinfo = featureinfo;
-			}
-		}*/
-		//variable to store requested feature info
-		var featureinfo = projectsSource.getGetFeatureInfoUrl(
-		 	    coordinate, viewResolution,'EPSG:3857', 
-		 	    {'INFO_FORMAT': 'text/html'});//return as json
-
-		//variable to store json object
-		//var myfeatureinfo = featureinfo.features;//returns an array containing the feature objects (one object in this case)
-
-
-		//function to get necessary field for the json object
+		var mapLayers = map.getLayers();
 		
-
-		//iterate through array and obtain required values from the object in array
-		/*var townId, townName;
-		for(var i = 0; i < featureinfo.features.length; i++){
-			townId = featureinfo.features[i].properties.town_id;
-			townName = featureinfo.features[i].properties.town_name;
-		}*/
-
-
-
-		//content.innerHTML = '<iframe seamless src="' + featureinfo + '"></iframe>' ;
+		
+	
+		//variable to store requested feature info
+		var featureinfo = allProjectsSource.getGetFeatureInfoUrl(
+		 	    coordinate, viewResolution,'EPSG:3857', 
+		 	    {'INFO_FORMAT': 'text/html'})
+		
 		content.innerHTML = '<iframe seamless src="' + featureinfo + '"></iframe>' ;
 		overlay.setPosition(coordinate);	
     });
